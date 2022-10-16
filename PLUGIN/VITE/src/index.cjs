b3da51f60f7bf9154487a7783054859ba58cf19a
senselogic <dev.senselogic@gmail.com>
2022-10-16 17:49:09 +0200

    + rollup

---
+++ PLUGIN/VITE/src/index.cjs
@@ -0,0 +1,49 @@
+// -- IMPORTS
+
+const fs = require( 'fs' ).promises;
+const createFilter = require( '@rollup/pluginutils' ).createFilter;
+const getProcessedCode = require( 'senselogic-reactor' );
+
+// -- FUNCTIONS
+
+function reactorViteLoader(
+    { framework = 'react', include = './src/*.jsx', exclude } = {}
+    )
+{
+    let filter = createFilter( include, exclude );
+
+    return {
+        name: 'senselogic-reactor-vite-loader',
+        enforce: 'pre',
+
+        async load(
+            path
+            )
+        {
+            var
+                code;
+
+            if ( filter( path ) )
+            {
+                try
+                {
+                    code = await fs.readFile( path, 'utf-8' );
+                }
+                catch ( exception )
+                {
+                    console.warn( `${path} can't be loaded by senselogic-reactor-vite-loader` );
+
+                    return;
+                }
+
+                code = getProcessedCode( code, { framework } );
+
+                return code;
+            }
+        }
+        };
+}
+
+// -- EXPORTS
+
+module.exports = reactorViteLoader;
